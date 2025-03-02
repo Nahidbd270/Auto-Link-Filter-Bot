be7ef9011a966f49aab02a839369961d612a5757
PrimeBotz <primemovies4unayem@gmail.com>
2025-03-03 05:50:59 +0600

    New Update 🥰 By TG @Prime_Botz

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/LazyUrlHunterrBOT.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
+git clone https://github.com/PrimeBotz/Link-Search-Prime-Botz.git /Link-Search-Prime-Botz
+cd /Link-Search-Prime-Botz
 pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
+echo "⚡️⚡️ Starting Link-Search-Prime-Botz..."
 python3 main.py
