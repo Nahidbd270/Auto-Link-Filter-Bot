f1411627768050087e44f7a741231666cb5e97f7
PrimeBotz <primemovies4unayem@gmail.com>
2025-03-03 06:01:52 +0600

    New Update 🥰 By TG @Prime_Botz

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'LazyURLhunter is Running ! Smile Baby'
+    return 'Link-Search-Prime-Botz is Running ! Smile Baby'
 
 
 if __name__ == "__main__":
