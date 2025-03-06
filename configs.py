fcde4f3cb7b0b431d08567ea01b3392915ca3877
Nahidbd270 <Nahidbd270@gmail.com>
2025-03-06 18:59:02 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,9 +10,9 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 19234664))
+    API_HASH = os.environ.get("API_HASH", "29c2f3b3d115cf1b0231d816deb271f5")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8070686038:AAGaXuAB1MZYGXuLUtZ5EeTh76nlA3DRoQc")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "Link-Search-Prime-Botz")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
